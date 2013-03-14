183226cced4010ce1c65e0e55b2887f07c6a68f3
Zach Wolf <hello@zachwolf.com>
2013-03-14 15:06:02 -0500

    It kind of worked!

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
 
 <!-- 
-    
+    hey
     /oooooooooooooooooooooooooooooooooooooooooooooo/
     .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh.
       +NMMMMyooooooooooooooooooooooooooooooyMMMMN+
