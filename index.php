d0df99a69255f9a2965236ea8dc1d1bb40836dd5
Zach Wolf <hello@zachwolf.com>
2013-03-14 15:51:50 -0500

    test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
 
 <!-- 
-    asdfasdfasdfasdfasdf
+    
     /oooooooooooooooooooooooooooooooooooooooooooooo/
     .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh.
       +NMMMMyooooooooooooooooooooooooooooooyMMMMN+
