c3ffe0c38ea2601dc5126e7fa4a98b19ab05ac08
Zach Wolf <hello@zachwolf.com>
2013-03-14 14:36:01 -0500

    Tests Github's dynamic pulling

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <meta charset="utf-8">
 
 <!-- 
-
+    git test
     /oooooooooooooooooooooooooooooooooooooooooooooo/
     .hMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMh.
       +NMMMMyooooooooooooooooooooooooooooooyMMMMN+
